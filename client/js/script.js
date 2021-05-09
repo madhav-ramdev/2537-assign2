80bc5feff7d091ecb53a79279227456ac95dd9ab
madhav-ramdev <mramdev1@my.bcit.ca>
2021-05-10 02:05:21 +0530

    abcd

---
+++ client/js/script.js
@@ -0,0 +1,351 @@
+$(document).ready(function () {
+    "use strict";
+
+    function getUsers() {
+        $.ajax({
+            url: "/get-users",
+            dataType: "json",
+            type: "GET",
+            success: function (data) {
+                console.log(data);
+                let newRow = `<table class= "table table-hover table-dark table-striped" id="users"><tr>
+                <th class="id_header"><span>ID</span></th>
+                <th class="name_header"><span>Name</span></th>
+                <th class="location_header"><span>Location</span></th>
+                <th class="gas_header"><span>Gas Vehicle (Yes or No)</span></th>
+                <th class="emissions_header"><span>Emission Count</span></th>
+                <th class="emissions_header"><span>Click to Delete</span></th>
+                </tr>`;
+
+                for (let i = 0; i < data.rows.length; i++) {
+                    let row = data.rows[i];
+                    newRow += ("<tr><td class='userID'>" + row.userID +
+                        "</td><td class='userName'>" + row.userName +
+                        "</td><td class='location'>" + row.location +
+                        "</td><td class='gasBool'><span>" + row.gasBool +
+                        "</span></td><td class='emission'>" + row.emission +
+                        "</td><td id = '" + row.userID + "' class='delBtn' onClick='clickFunction(" + row.userID + ")'><span>╳</span> </td></tr>");
+                }
+                newRow += "</table>";
+                $("#container").html(newRow);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#p2").text(jqXHR.statusText);
+                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            }
+        });
+    }
+    getUsers();
+
+    $('#submit-btn').click(function (e) {
+        e.preventDefault();
+
+        let formData = {
+            userID: $("#userID").val(),
+            userName: $("#userName").val(),
+            location: $("#location").val(),
+            gasBool: $("#gasBool").val(),
+            emission: $("#emission").val()
+        };
+        $("#userID").val("");
+        $("#userName").val("");
+        $("#location").val("");
+        $("#gasBool").val("");
+        $("#emission").val("");
+
+        $.ajax({
+            url: "/add-user",
+            dataType: "json",
+            type: "POST",
+            data: formData,
+            success: function (data) {
+                $("#requestStatus").html("The user's data was added to the database.");
+                getUsers();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#p2").text(jqXHR.statusText);
+                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            }
+        });
+    });
+
+    // --------------------------- DELETE FUNCTIONS START HERE -------------------------
+
+    $('#deleteRecord1').click(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "/delete-Record-1",
+            dataType: "json",
+            type: "POST",
+            success: function (data) {
+                console.log(data);
+                $("#status").html("Record 1 deleted.");
+                getUsers();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#p2").text(jqXHR.statusText);
+                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            }
+        });
+    });
+
+    $('#deleteRecord2').click(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "/delete-Record-2",
+            dataType: "json",
+            type: "POST",
+            success: function (data) {
+                console.log(data);
+                $("#status").html("Record 2 deleted.");
+                getUsers();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#p2").text(jqXHR.statusText);
+                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            }
+        });
+    });
+
+    $('#deleteRecord3').click(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "/delete-Record-3",
+            dataType: "json",
+            type: "POST",
+            success: function (data) {
+                console.log(data);
+                $("#status").html("Record 3 deleted.");
+                getUsers();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#p2").text(jqXHR.statusText);
+                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            }
+        });
+    });
+
+    $('#deleteRecord4').click(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "/delete-Record-4",
+            dataType: "json",
+            type: "POST",
+            success: function (data) {
+                console.log(data);
+                $("#status").html("Record 4 deleted.");
+                getUsers();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#p2").text(jqXHR.statusText);
+                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            }
+        });
+    });
+
+    $('#deleteRecord5').click(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "/delete-Record-5",
+            dataType: "json",
+            type: "POST",
+            success: function (data) {
+                console.log(data);
+                $("#status").html("Record 5 deleted.");
+                getUsers();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#p2").text(jqXHR.statusText);
+                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            }
+        });
+    });
+
+    $('#deleteRecord6').click(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "/delete-Record-6",
+            dataType: "json",
+            type: "POST",
+            success: function (data) {
+                console.log(data);
+                $("#status").html("Record 6 deleted.");
+                getUsers();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#p2").text(jqXHR.statusText);
+                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            }
+        });
+    });
+
+    $('#deleteRecord7').click(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "/delete-Record-7",
+            dataType: "json",
+            type: "POST",
+            success: function (data) {
+                console.log(data);
+                $("#status").html("Record 7 deleted.");
+                getUsers();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#p2").text(jqXHR.statusText);
+                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            }
+        });
+    });
+
+    $('#deleteRecord8').click(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "/delete-Record-8",
+            dataType: "json",
+            type: "POST",
+            success: function (data) {
+                console.log(data);
+                $("#status").html("Record 8 deleted.");
+                getUsers();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#p2").text(jqXHR.statusText);
+                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            }
+        });
+    });
+
+    $('#deleteRecord9').click(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "/delete-Record-9",
+            dataType: "json",
+            type: "POST",
+            success: function (data) {
+                console.log(data);
+                $("#status").html("Record 9 deleted.");
+                getUsers();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#p2").text(jqXHR.statusText);
+                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            }
+        });
+    });
+
+    $('#deleteRecord10').click(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: "/delete-Record-10",
+            dataType: "json",
+            type: "POST",
+            success: function (data) {
+                console.log(data);
+                $("#status").html("Record 10 deleted.");
+                getUsers();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#p2").text(jqXHR.statusText);
+                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            }
+        });
+    });
+
+    $('#container').on('click', 'span', function () {
+
+        if ($(this).parent().attr('class') === 'gasBool') {
+            let spanText = $(this).text();
+            let td = $(this).parent();
+            let input = $("<input type='text' value='" + spanText + "'>");
+
+            td.html(input);
+            $(input).keyup(function (e) {
+                let val = null;
+                let span = null;
+                if (e.which == 13) {
+                    val = $(input).val();
+                    span = $("<span>" + val + "</span>");
+                    td.html(span);
+
+                    console.log(td.parent().find("[class='userID']")[0]);
+
+                    let dataToServer = {
+                        id: td.parent().find("[class='userID']").html(),
+                        gasBool: val
+                    };
+
+                    $.ajax({
+                        url: "update-user",
+                        dataType: "json",
+                        type: "POST",
+                        data: dataToServer,
+                        success: function (data) {
+                            $("#requestStatus").html("The data was updated on the database.");
+                            getUsers();
+                        },
+                        error: function (jqXHR, textStatus, errorThrown) {
+                            $("#p2").text(jqXHR.statusText);
+                            console.log("ERROR:", jqXHR, textStatus, errorThrown);
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+    // $('#submit-btn').click(function (e) {
+    //     e.preventDefault(); 
+    function clickFunction(clicked_id) {
+        let variableX = clicked_id;
+
+        // if ($(this).parent().attr('class') === 'delBtn') {
+        //     // let spanText = $(this).text();
+        //     let td = $(this).parent();
+        //     // let input = $("<input type='button' value='delete'>");
+
+        //     // td.html(input);
+        //     $(this).keyup(function (e) {
+        //         let val = null;
+        //         let span = null;
+        // if (e.which == 46) {
+        // val = $(input).val();
+        // span = $("<span>" + val + "</span>");
+        // td.html(span);
+
+        // console.log(td.parent().find("[class='userID']")[0]);
+
+        let dataToServer = {
+            id: variableX
+            // id: this.parent().find("[class='delBtn']"),
+            // gasBool: val
+        };
+
+        $.ajax({
+            url: "delete-user",
+            dataType: "json",
+            type: "POST",
+            data: dataToServer,
+            success: function (data) {
+                $("#requestStatus").html("The data was deleted on the database.");
+                getUsers();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $("#p2").text(jqXHR.statusText);
+                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            }
+        });
+        // }
+        // });
+        // }
+    };
+});
---
+++ server.js
@@ -0,0 +1,218 @@
+"use strict";
+// REQUIRES
+const express = require('express');
+const app = express();
+const fs = require("fs");
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+
+
+const msg404 = 'Err!! We could not find what you are looking for. Please try again';
+
+app.use('/js', express.static('client/js'))
+app.use('/css', express.static('client/css'))
+app.use('/img', express.static('client/img'))
+
+
+
+
+app.get('/', function (req, res) {
+
+
+  // Let's build the DB if it doesn't exist
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    multipleStatements: true
+  });
+
+  const createDBAndTables = `CREATE DATABASE IF NOT EXISTS assignment;
+        use assignment;
+        CREATE TABLE IF NOT EXISTS appUser (
+        userID int NOT NULL AUTO_INCREMENT,
+        userName varchar(40),
+        location varchar(30),
+        gasBool varchar(30),
+        emission int(10),
+        PRIMARY KEY (userID));
+        INSERT INTO appUser (userName, location, gasBool, emission) values ('madhav', 'vancouver', 'no', '23');
+        INSERT INTO appUser (userName, location, gasBool, emission) values ('sam', 'burnaby', 'yes', '27');
+        INSERT INTO appUser (userName, location, gasBool, emission) values ('john', 'surrey', 'no', '12');
+        INSERT INTO appUser (userName, location, gasBool, emission) values ('harleen', 'vancouver', 'no', '25');
+        INSERT INTO appUser (userName, location, gasBool, emission) values ('aaron', 'richmond', 'yes', '32');
+        INSERT INTO appUser (userName, location, gasBool, emission) values ('dustin', 'north vancouer', 'yes', '23');
+        INSERT INTO appUser (userName, location, gasBool, emission) values ('henry', 'vanouver', 'yes', '14');
+        INSERT INTO appUser (userName, location, gasBool, emission) values ('scott', 'downtown', 'no', '87');
+        INSERT INTO appUser (userName, location, gasBool, emission) values ('smith', 'burnaby', 'no', '27');
+        INSERT INTO appUser (userName, location, gasBool, emission) values ('justin', 'surrey', 'yes', '23');`;
+
+  connection.connect();
+  connection.query(createDBAndTables, function (error, results, fields) {
+    if (error) {
+      throw error;
+    }
+
+  });
+  connection.end();
+
+  let doc = fs.readFileSync('./client/html/index.html', "utf8");
+  res.send(doc);
+});
+
+
+app.get('/get-users', function (req, res) {
+
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'assignment'
+  });
+  connection.connect();
+  connection.query('SELECT * FROM appUser', function (error, results, fields) {
+    if (error) {
+      throw error;
+    }
+    console.log('Rows returned are: ', results);
+    res.send({
+      status: "success",
+      rows: results
+    });
+
+  });
+  connection.end();
+
+
+});
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Notice that this is a 'POST'
+app.post('/add-user', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+
+  console.log("username", req.body.userName);
+  console.log("location", req.body.location);
+  console.log("gas vehicle", req.body.gasBool);
+  console.log("emissions", req.body.emission);
+
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'assignment'
+  });
+  connection.connect();
+
+  connection.query('INSERT INTO appUser (userID, userName, location, gasBool, emission) values (?, ?, ?, ?, ?)',
+    [req.body.userID, req.body.userName, req.body.location, req.body.gasBool, req.body.emission],
+    function (error, results, fields) {
+      if (error) {
+        throw error;
+      }
+      console.log('Rows returned are: ', results);
+      res.send({
+        status: "success",
+        msg: "Recorded added."
+      });
+
+    });
+  connection.end();
+
+});
+
+app.post('/delete-all-users', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'assignment'
+  });
+  connection.connect();
+  // REALLY A DUMB THING TO DO, BUT JUST SHOWING YOU CAN
+  connection.query('DELETE FROM appUser',
+    function (error, results, fields) {
+      if (error) {
+        throw error;
+      }
+      //console.log('Rows returned are: ', results);
+      res.send({
+        status: "success",
+        msg: "Recorded all deleted."
+      });
+
+    });
+  connection.end();
+
+});
+
+app.post('/update-user', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'assignment'
+  });
+  connection.connect();
+
+  connection.query('UPDATE appUser SET gasBool = ? WHERE userID = ?',
+    [req.body.gasBool, req.body.id],
+    function (error, results, fields) {
+      if (error) {
+        throw error;
+      }
+      console.log('Rows returned are: ', results);
+      res.send({
+        status: "success",
+        msg: "Recorded updated."
+      });
+
+    });
+  connection.end();
+
+});
+
+app.post('/delete-user', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+
+  let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'assignment'
+  });
+  connection.connect();
+
+  connection.query('DELETE FROM appUser WHERE userID = ?',
+    [req.body.userID],
+    function (error, results, fields) {
+      if (error) {
+        throw error;
+      }
+      // console.log('Rows returned are: ', results);
+      res.send({
+        status: "success",
+        msg: "Recorded deleted."
+      });
+
+    });
+  connection.end();
+
+});
+
+// RUN SERVER
+let port = 8000;
+app.listen(port, function () {
+  console.log('App listening on port ' + port + '!');
+})
